7b554a782f048579e25c18f5dc81af95b14960bc
Praveen Solanki <praveensolanki9953@gmail.com>
2023-05-10 09:06:50 +0530

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <p>© 2023 Th. PS HeadPhones </p>
+      <p className='icons'>
+        <AiFillInstagram/>
+        <AiOutlineTwitter/>
+
+      </p>
+    </div>
+  )
+}
+
+export default Footer
